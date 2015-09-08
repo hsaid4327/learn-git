2f7ef6e3702cc59905d455630bb64739a37dc83f
Hammad Said <hsaid-contractor@M-20013300.local>
2015-09-08 13:46:54 -0700

    The collections package added for collection related classes

---
+++ core-java/java-collections/src/main/java/com/mine/core/ArrayListTest.java
@@ -0,0 +1,50 @@
+package com.mine.core;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+
+public class ArrayListTest {
+	
+	public static void main(String args[]){
+		List<String> colors = new ArrayList<>();
+		String[] colorsToAdd = {"Blue","Green", "Yellow", "Magenta"};
+		String [] colorsToRemove = {"Blue", "Green"};
+		
+		List<String> removeList = new ArrayList<>();
+		
+		for(String toAdd: colorsToAdd){
+			colors.add(toAdd);
+		}
+				
+		for(String toRemove: colorsToRemove){
+			removeList.add(toRemove);
+		}
+		
+		removeColors(colors, removeList);
+	}
+
+	private static void removeColors(List<String> colors,
+			List<String> removeList) {
+		
+         System.out.println("This is the list of colors");
+         System.out.println(colors);
+         
+         System.out.println("This is the list of colors to remove");
+         System.out.println(removeList);
+         
+         Iterator<String> it = colors.iterator();
+         while (it.hasNext()){
+        	 String color = it.next();
+        	 if(removeList.contains(color)){
+        		 it.remove();
+        		 
+        	 }
+         }
+		System.out.println("The list after removal");
+		System.out.println(colors);
+	}
+        
+}
---
+++ core-java/java-collections/src/main/java/com/mine/core/LinkedListTest.java
@@ -0,0 +1,69 @@
+package com.mine.core;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+		String[] colors = {"black", "yellow", "green", "blue", "violet", "silver"};
+		List<String> list1 = new LinkedList<>();
+		
+		for(String color: colors){
+			list1.add(color);
+		}
+		
+		String[] colors2 = {"gold", "white", "brown", "blue", "gray", "silver"};
+		
+		List<String> list2 = new LinkedList<>();
+		
+		for(String color: colors2){
+			list2.add(color);
+			
+		}
+		list1.addAll(list2);
+		printList(list1);
+		toUpperCase(list1);
+		printList(list1);
+		printReverse(list1);
+		removeList(list1, 4,7);
+		printList(list1);
+
+	}
+
+	private static void removeList(List<String> list1, int i, int j) {
+		list1.subList(i, j).clear();
+		
+	}
+
+	private static void printReverse(List<String> list1) {
+		ListIterator<String> it = list1.listIterator(list1.size());
+		while(it.hasPrevious()){
+			System.out.printf(" %s ", it.previous());
+		}
+		
+	}
+
+	private static void toUpperCase(List<String> list1) {
+       for(int i=0; i < list1.size(); i++){
+    	   String color = list1.get(i);
+    	   list1.set(i, color.toUpperCase());
+    	   
+       }
+		
+		
+	}
+
+	private static void printList(List<String> list1) {
+		System.out.printf("%nlist:%n");
+		
+		for(String color: list1){
+			System.out.printf(" %s ", color);
+		}
+		System.out.printf("%n");
+		
+	}
+
+}
